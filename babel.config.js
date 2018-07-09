5b651a3943552743889beea1a0dbc216c0b4db6c
Alexey Ozerov <alexozerov@gmail.com>
2018-07-09 15:15:54 +0300

    WIP: should work

--- babel.config.js
+++ babel.config.js
@@ -1,29 +1,47 @@
 module.exports = {
-  "presets": [
-    "@babel/env",
-    "@babel/preset-react",
-    "@babel/flow"
+  'presets': [
+    '@babel/env',
+    '@babel/preset-react',
+    '@babel/flow'
   ],
-  "sourceMaps": true,
-  "plugins": [
-    "@babel/plugin-transform-flow-strip-types",
-    ["@babel/plugin-transform-runtime", {
-      "polyfill": false,
-      "regenerator": true
-    }],
-    ["@babel/plugin-proposal-decorators", { "legacy": true }],
-    "@babel/plugin-proposal-class-properties",
-    "functional-hmr",
-    "transform-new-target",
-    "transform-exponentiation-operator",
-    ["babel-plugin-transform-builtin-extend", {
-      "globals": ["Error", "Array"]
-    }],
-    ["transform-inline-environment-variables", {
-      "include": [
-        "NODE_ENV",
-        "REDUX_LOGGER"
-      ]
-    }]
+  'sourceMaps': true,
+  'plugins': [
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-transform-exponentiation-operator',
+    '@babel/plugin-transform-flow-strip-types',
+    '@babel/plugin-transform-new-target',
+    '@babel/plugin-transform-react-jsx-source',
+    'functional-hmr',
+    [
+      '@babel/plugin-transform-runtime',
+      {
+        'polyfill': false,
+        'regenerator': true,
+      }
+    ],
+    [
+      '@babel/plugin-proposal-decorators',
+      {
+        'legacy': true
+      }
+    ],
+    [
+      'babel-plugin-transform-builtin-extend',
+      {
+        'globals': [
+          'Error',
+          'Array'
+        ]
+      }
+    ],
+    [
+      'transform-inline-environment-variables',
+      {
+        'include': [
+          'NODE_ENV',
+          'REDUX_LOGGER'
+        ]
+      }
+    ]
   ]
 }
