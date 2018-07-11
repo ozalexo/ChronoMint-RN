3a255c3efb30ab59de2e3c4b7b80f9e6837231c7
Alexey Ozerov <alexozerov@gmail.com>
2018-07-11 14:49:08 +0300

    Remove unnecessary dependencies from XCode project
    Removed libRNFS.a after removing of NPM package react-native-fs from dependencies
    Also formatted file babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,35 +1,36 @@
 module.exports = {
-  presets: [
+  'presets': [
     '@babel/env',
     '@babel/preset-react',
     '@babel/flow'
   ],
-  env: {
-    test: {
-      presets: [
+  'env': {
+    'test': {
+      'presets': [
         ['@babel/env', { modules: false }],
-        '@babel/preset-react', '@babel/flow'
+        '@babel/preset-react',
+        '@babel/flow'
       ],
-      plugins: [
+      'plugins': [
         '@babel/plugin-syntax-dynamic-import',
         '@babel/plugin-transform-modules-commonjs'
       ],
     },
   },
-  babelrcRoots: 'packages/*',
-  sourceMaps: true,
-  plugins: [
+  'babelrcRoots': 'packages/**',
+  'sourceMaps': true,
+  'plugins': [
     [
       '@babel/plugin-transform-runtime',
       {
-        helpers: false,
-        polyfill: false,
-        regenerator: true,
-        moduleName: '@babel/runtime',
+        'helpers': false,
+        'polyfill': false,
+        'regenerator': true,
+        'moduleName': '@babel/runtime',
       },
     ],
     '@babel/plugin-transform-flow-strip-types',
-    ['@babel/plugin-proposal-decorators', { legacy: true }],
+    ['@babel/plugin-proposal-decorators', { 'legacy': true }],
     '@babel/plugin-proposal-class-properties',
     'functional-hmr',
     'transform-new-target',
@@ -37,13 +38,13 @@ module.exports = {
     [
       'babel-plugin-transform-builtin-extend',
       {
-        globals: ['Error', 'Array'],
+        'globals': ['Error', 'Array'],
       },
     ],
     [
       'transform-inline-environment-variables',
       {
-        include: ['NODE_ENV', 'REDUX_LOGGER'],
+        'include': ['NODE_ENV', 'REDUX_LOGGER'],
       },
     ],
   ],
