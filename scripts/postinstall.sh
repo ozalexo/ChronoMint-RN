209e5e9a23ef6e6a237df6cd4c42658d8b3d6f71
Alexey Ozerov <alexozerov@gmail.com>
2018-10-26 16:02:53 +0300

    CM-144 Android: to complete SplashScreen integration

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,6 +1,12 @@
     #!/usr/bin/env bash
-    
-    cd node_modules/react-native
-    scripts/ios-install-third-party.sh
-    cd third-party/glog-0.3.5/
+
+    # "Solve" one of known issues (using XCode 10 and `react-native init`)
+    # https://github.com/react-native-community/react-native-releases/blob/master/CHANGELOG.md#known-issues
+
+    ROOT_PWD=$(pwd)
+    cd ./node_modules/react-native
+    ./scripts/ios-install-third-party.sh
+    cd ./third-party/glog-0.3.5/
     ../../scripts/ios-configure-glog.sh
+    cd $ROOT_PWD
+
