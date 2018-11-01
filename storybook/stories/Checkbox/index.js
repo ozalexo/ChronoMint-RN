87d93ee54002f8803c46ac1c999e16610fb55855
e-molochko <molegorchik@gmail.com>
2018-11-01 18:08:10 +0300

    Added Checkbox and Cautions

---
+++ storybook/stories/Checkbox/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Checkbox from '../../../src/components/Checkbox';
+
+export default function StoryCheckbox({ label, isDark, isChecked, onPress }) {
+  return <Checkbox label={label} isDark={isDark} isChecked={isChecked} onPress={onPress} />;
+}
+
+StoryCheckbox.defaultProps = {
+    label: 'Text label',
+    isDark: true,
+    isChecked: false,
+    onPress: () => {},
+};
+
+StoryCheckbox.propTypes = {
+    label: PropTypes.string,
+    isDark: PropTypes.bool,
+    isChecked: PropTypes.bool,
+    onPress: PropTypes.func,
+};
