7afeac8c2df88731a7098c30e30deceb7b3aa3a0
e-molochko <molegorchik@gmail.com>
2018-11-16 13:47:17 +0300

    Added GenerateMnemonic Screen

--- src/screens/Login/GenerateMnemonic/GenerateMnemonicContainer.js
+++ src/screens/Login/GenerateMnemonic/GenerateMnemonicContainer.js
@@ -4,15 +4,57 @@
  */
 
 import React, { PureComponent } from 'react'
+import { generateMnemonic } from '@chronobank/bitcoin/utils'
+import PropTypes from 'prop-types'
 import GenerateMnemonic from './GenerateMnemonic'
 
 class GenerateMnemonicContainer extends PureComponent {
-  render () {
 
+  state = {
+    mnemonic: '',
+  }
+
+  componentDidMount () {
+    generateMnemonic()
+      .then(resolve => {
+        this.setState({ mnemonic: resolve })
+      })
+  }
+
+
+  handleConfirm = () => {
+    const {
+      password,
+      privateKey,
+      navigation,
+    } = this.props
+    const { mnemonic } = this.state
+
+    const params = {
+      mnemonic,
+      password,
+      privateKey
+    }
+
+    navigation.navigate('ConfirmMnemonic', params)
+
+  }
+
+  render () {
+    const { mnemonic } = this.state
     return (
-      <GenerateMnemonic />
+      <GenerateMnemonic
+        mnemonic={mnemonic}
+        onConfirm={this.handleConfirm}
+      />
     )
   }
+}
+
+GenerateMnemonicContainer.propTypes = {
+  mnemonic: PropTypes.string,
+  password: PropTypes.string,
+  privateKey: PropTypes.string,
 }
 
 export default GenerateMnemonicContainer
--- src/screens/Login/GenerateMnemonic/GenerateMnemonicStyles.js
+++ src/screens/Login/GenerateMnemonic/GenerateMnemonicStyles.js
@@ -8,10 +8,67 @@ import { headerHeight } from '../../../common/constants/screens'
 
 export default StyleSheet.create({
   screenView: {
-    marginTop: headerHeight + 20,
-    marginBottom: 20,
+    marginTop: headerHeight,
     flex: 1,
     flexDirection: 'column',
+  },
+  description: {
+    color: '#A3A3CC',
+    fontSize: 16,
+    margin: 20,
+  },
+  mnemonic: {
+    borderRadius: 3,
+    color: '#FFB54E',
+    fontSize: 16,
+    fontWeight: '900',
+  },
+  mnemonicContainer: {
+    backgroundColor: '#302D59',
+    marginHorizontal: 20,
+    marginVertical: 10,
     paddingHorizontal: 20,
+    paddingVertical: 30,
+  },
+  primaryButton: {
+    marginHorizontal: '20%',
+  },
+  warningContainer: {
+    backgroundColor: '#302D59',
+    borderRadius: 3,
+    borderTopColor: '#FFB54E',
+    borderTopWidth: 5,
+    marginHorizontal: 20,
+    marginVertical: 10,
+    paddingBottom: 30,
+  },
+  warningItem: {
+    flexDirection: 'row',
+    marginHorizontal: 20,
+    marginVertical: 10,
+  },
+  warningItemContent: {
+    color: '#A3A3CC',
+    flex: 1,
+    fontSize: 16,
+  },
+  warningItemTitle: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: '900',
+  },
+  warningNumber: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: '900',
+    marginRight: 5,
+  },
+  warningTitle: {
+    color: 'white',
+    fontSize: 22,
+    fontWeight: '900',
+    marginBottom: 10,
+    marginHorizontal: 20,
+    marginTop: 30,
   },
 })
