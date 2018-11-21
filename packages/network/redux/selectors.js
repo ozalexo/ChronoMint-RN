6b4e2de151b2390d28c1aa54ecc1d2d5c47b5169
Alexey Ozerov <alexozerov@gmail.com>
2018-11-21 16:28:52 +0300

    CM-258: network - selectors and clients mappings

--- packages/network/redux/selectors.js
+++ packages/network/redux/selectors.js
@@ -1,4 +1,50 @@
-/**
+ /**
  * Copyright 2017–2018, LaborX PTY
  * Licensed under the AGPL Version 3 license.
  */
+
+import { createSelector } from 'reselect'
+import { DUCK_NETWORK } from './constants'
+
+const flatten = (list) => list.reduce(
+  (a, b) => a.concat(Array.isArray(b) ? flatten(b) : b), []
+)
+
+/**
+ * get network
+ * @param {object} state
+ */
+export const getDuckNetwork = (state) =>
+  state[DUCK_NETWORK]
+
+/**
+ * get network.selected
+ * @param {object} state
+ */
+export const getCurrentNetwork = createSelector(
+  getDuckNetwork,
+  (network) => network.selected
+)
+
+/**
+ * get network.availableNetworks
+ * @param {object} state
+ */
+export const getAvailableNetworks = createSelector(
+  getDuckNetwork,
+  (network) => network.availableNetworks
+)
+
+
+/**
+ * get flattened network.displaySections.%section%.networks
+ * @param {object} state
+ */
+export const getAvailableNetworkList = createSelector(
+  getDuckNetwork,
+  (network) => {
+    const list = network.displaySections
+      .map((item) => item.networks)
+    return flatten(list)
+  }
+)
