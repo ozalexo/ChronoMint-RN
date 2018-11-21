ba4e10811c395dc50d3008527f46b0508fef98c2
e-molochko <molegorchik@gmail.com>
2018-11-21 14:42:13 +0300

    CM-259 - Added privateKey generation by mnemonic phrase - Fixed getting address by privateKey

--- packages/bitcoin/utils/index.js
+++ packages/bitcoin/utils/index.js
@@ -3,20 +3,7 @@
  * Licensed under the AGPL Version 3 license.
  */
 
-import Mnemonic from 'bitcore-mnemonic-react-native'
 import bitcoin, { networks } from 'bitcoinjs-lib'
-
-const mnemonicObject = new Mnemonic(Mnemonic.Words.ENGLISH)
-
-export const generateMnemonic = async () => {
-  const phrase = await mnemonicObject.phrase
-  return phrase
-}
-
-export const getPrivateKey = async () => {
-  const key = mnemonicObject.toHDPrivateKey()
-  return key
-}
 
 export const getAddress = (privateKey) => {
   const keyPair = getKeyPair(privateKey)
