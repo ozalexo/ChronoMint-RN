6503b2660136986b3ecdcaf2eb5314fe892c9b5f
Alexey Ozerov <alexozerov@gmail.com>
2018-11-21 17:45:02 +0300

    Copied Web3 middleware from hisbi

---
+++ packages/ethereum/middleware/initialState.js
@@ -0,0 +1,23 @@
+/**
+ * Copyright 2017–2018, LaborX PTY
+ * Licensed under the AGPL Version 3 license.
+ */
+
+export default {
+  web3Listener: {
+    isConnected: false,
+    isConnecting: false,
+    isSyncing: null,
+    contracts: {
+      list: [],
+    },
+  },
+  web3Writer: {
+    isConnected: false,
+    isConnecting: false,
+    isSyncing: null,
+    contracts: {
+      list: [],
+    },
+  },
+}
