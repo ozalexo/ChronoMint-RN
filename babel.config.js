6ab9a13a9fe604f39bc14c65b068b7610ccb28e8
Alexey Ozerov <alexozerov@gmail.com>
2018-11-21 12:48:12 +0300

    Babel: enabled 'export * as' syntax

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,12 @@
 module.exports = function (api) {
   api.cache(true)
-  const presets = ['module:metro-react-native-babel-preset']
+  const presets = [
+    'module:metro-react-native-babel-preset',
+  ]
   const plugins = [
     '@babel/plugin-transform-flow-strip-types',
+    '@babel/plugin-proposal-export-default-from',
+    '@babel/plugin-proposal-export-namespace-from',
     '@babel/plugin-proposal-class-properties',
     '@babel/plugin-transform-regenerator',
     '@babel/plugin-transform-async-to-generator',
