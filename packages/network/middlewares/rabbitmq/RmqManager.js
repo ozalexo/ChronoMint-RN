dd74f76cdeac49b878614fd129f83c111cdfdc8b
Alexey Ozerov <alexozerov@gmail.com>
2019-01-15 19:58:13 +0300

    RabbitMQ: base URL getting from Redux state. (#94)
    * RabbitMQ: base URL getting from Redux state.
    
    connect thunks of RMQ get it directly from Redux store
    During reconnect by NETWORK_SELECT it should obtain new URL automatically (to be implemented and tested in other tasks)
    
    * CM-413 fix stomp connections for android

--- packages/network/middlewares/rabbitmq/RmqManager.js
+++ packages/network/middlewares/rabbitmq/RmqManager.js
@@ -3,7 +3,6 @@
  * Licensed under the AGPL Version 3 license.
  */
 
-import SockJS from 'sockjs-client'
 import webstomp from 'webstomp-client'
 
 class RmqManager {
@@ -18,7 +17,7 @@ class RmqManager {
     this.client && this.ws && this.disconnect()
     return new Promise((resolve, reject) => {
       try {
-        this.ws = new SockJS(baseUrl)
+        this.ws = new WebSocket(baseUrl)
       } catch (error) {
         return reject(error.message)
       }
@@ -26,6 +25,7 @@ class RmqManager {
       this.client = webstomp.over(this.ws, {
         heartbeat: false,
         debug: false,
+        binary: true,
       })
 
       this.client.connect(
